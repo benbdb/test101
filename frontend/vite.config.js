d6adf5800a5151aef99e912901060adfc5158d51
Jithin Cyriac <103352020+OptimusNib@users.noreply.github.com>
2025-10-01 17:23:50 +1000

    feat: frontend integration to backend

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/login': 'http://localhost:3000',
+      '/logout': 'http://localhost:3000',
+      '/callback': 'http://localhost:3000',
+      '/me': 'http://localhost:3000',
+      '/upload-video': 'http://localhost:3000',
+      '/download-video': 'http://localhost:3000',
+      '/list-videos': 'http://localhost:3000'
+    }
+  }
+})
